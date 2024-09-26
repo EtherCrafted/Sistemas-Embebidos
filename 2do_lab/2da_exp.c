2d499e36e3d5800da6052ab7c1cb85b4da202008
EtherCrafted <193838@unsaac.edu.pe>
2024-09-26 09:12:39 -0500

    Comentarios
    Se comento las lineas de codigo

--- 2do_lab/2da_exp.c
+++ 2do_lab/2da_exp.c
@@ -1,9 +1,10 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include "util/delay.h"
 
-unsigned contador = 0;
-unsigned decenas = 0;
-unsigned unidades = 0;
+unsigned contador = 0;  // Variable contador
+unsigned decenas = 0;   // Variable decenas de contador
+unsigned unidades = 0;  // Variable unidades de contador
 // decodificador para un display de 7 segmentos
 const  uint8_t buff_7seg[10] = {
     0x3F,   //0b0011 1111    0
@@ -19,11 +20,11 @@ const  uint8_t buff_7seg[10] = {
 };
 //funcion con la configuracion de puertos
 void fun_puertos() {
-    DDRD = (DDRD & 0xF0) | 0x03; // xxxx 0011 PD2-PD3 conf. como entrada PD0-PD1 conf. como salida 
-    DDRB |= 0x3F;   // xx11 1111 PB0-PB5 configurados como salida (6 bit Disp_7seg)
-    DDRC |= 0x01;   // xxxx xxx1 PC0 configurado como salida (1 bit Disp_7seg)
+    DDRD = (DDRD & 0xF0) | 0x03; // xxxx 0011 PD2-PD3 entrada PD0-PD1 salidas 
+    DDRB |= 0x3F;   // xx11 1111 PB0-PB5 salidas (6 bits Disp_7seg)
+    DDRC |= 0x01;   // xxxx xxx1 PC0 salida (1 bit Disp_7seg)
 }
-
+//funcion con la configuracion de inicialisacion
 void fun_init() {
     PORTB &= 0xC0; //PB inicializado en 0.
     PORTC &= 0xFE; //PC0 inicializado en 0.
@@ -42,7 +43,7 @@ void fun_7seg(uint8_t numero) {
         PORTC &= ~(1 << 0); // Establece el bit C0 en 0
     }
 }
-
+//funcion con la configuracion de interrupciones
 void fun_interrupsiones() {
     EICRA = 0b00001010; //Interrupción externa por flanco de bajada en INT0 e INT1
     EIMSK = 0b00000011; //Habilita interrupción externa en INT0 y INT1
@@ -52,12 +53,12 @@ void fun_interrupsiones() {
 
 //Interrupción por flanco de bajada en INT1
 ISR(INT1_vect) {
-    contador++;
+    contador++;     // Incrementa el contador cuando hay una interrupcion en INT1
 }
 
 //Interrupción por flanco de bajada en INT0
 ISR(INT0_vect) {
-    contador--;
+    contador--;     // Decrementa el contador cuando hay una interrupcion en INT0
 }
 
 //-------------------- Main Program -------------------------
@@ -66,13 +67,15 @@ int main(void) {
     fun_init();
     fun_interrupsiones();
     while(1) {
-        decenas = contador / 10;
-        unidades = contador % 10;
-        PORTD |= (1 << PD0);
-        fun_7seg(unidades)
-        PORTD &= ~(1 << PD0);
-        PORTD |= (1 << PD1);
-        fun_7seg(decenas)
-        PORTD &= ~(1 << PD1);
+        decenas = contador / 10;    // Descomposicion en decenas
+        unidades = contador % 10;   // Descomposicion en unidades
+        PORTD |= (1 << PD0);        // Habilita el diplay de 7 seg. de unidades
+        fun_7seg(unidades)          // Muestra el valor de unidades en el display
+        _delay_ms(100);             // Retraso para que se visualice correctamente
+        PORTD &= ~(1 << PD0);       // Deshabilita el diplay de 7 seg. de unidades
+        PORTD |= (1 << PD1);        // Habilita el diplay de 7 seg. de decenas
+        fun_7seg(decenas)           // Muestra el valor de decenas en el display
+        _delay_ms(100);             // Retraso para que se visualice correctamente
+        PORTD &= ~(1 << PD1);       // Deshabilita el diplay de 7 seg. de decenas
     }
 }
