b31856be3a41e69176ec3264fe04cb5908929ad8
EtherCrafted <193838@unsaac.edu.pe>
2024-10-11 09:26:06 -0500

    info_3

--- 3er_lab/1er_exp.c
+++ 3er_lab/1er_exp.c
@@ -1,24 +1,79 @@
-#define F_CPU 16000000  //frecuencias del Atmega328p
-// Librerias
-#include <avr/io.h>
-#include "util/delay.h"
+/**
+  Generated main.c file from MPLAB Code Configurator
 
-void fun_puertos() {
-    DDRB |= 0x2A;   // xx1x 1x1x PB1,PB3,PB5 configurados como salida (leds)
-    DDRB &= ~(1 << PB0); // xx1x 1x10 PB0 como entrada (boton)
-}
+  @Company
+    Microchip Technology Inc.
 
-//Main
-int main(void) {
-    fun_puertos();  // Se llama a la funcion que contiene la configuracion de puertos
-    while (1) {
-        if (PINB & (1 << PB0)){     // Verificar si se pulso el boton (pull up)   
-            PORTB &= 0xD5;          // Salida de los leds 000
-        }
-        else{
-             PORTB |= 0x2A;         // Salida de los leds 111
-        }
+  @File Name
+    main.c
+
+  @Summary
+    This is the generated main.c using PIC24 / dsPIC33 / PIC32MM MCUs.
+
+  @Description
+    This source file provides main entry point for system initialization and application code development.
+    Generation Information :
+        Product Revision  :  PIC24 / dsPIC33 / PIC32MM MCUs - 1.171.4
+        Device            :  PIC24FJ256GB110
+    The generated drivers are tested against the following:
+        Compiler          :  XC16 v2.10
+        MPLAB 	          :  MPLAB X v6.05
+*/
+
+/*
+    (c) 2020 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
+*/
+
+/**
+  Section: Included Files
+*/
+#include "mcc_generated_files/system.h"
+
+/*
+                         Main application
+ */
+int main(void)
+{
+    // initialize the device
+    SYSTEM_Initialize();
+
+    while (1)
+    {
+    if (cont == 255)
+    {
+      cont = 0;
     }
-}
+    else{
+      cont++
+      if (cont%5 == 0)
+      {
+        _latCsda==;
+      }
+       __delay_ms(100);
+    }
+    }
 
+    return 1;
+}
+/**
+ End of File
+*/
 
